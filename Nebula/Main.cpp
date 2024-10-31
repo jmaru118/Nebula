39647f2b9768f546c82c0f137446caf947419639
jmaru118 <jonatmartinez@icloud.com>
2024-10-30 18:47:16 -0700

    added fragment shader and vertex shader

--- Nebula/Main.cpp
+++ Nebula/Main.cpp
@@ -2,6 +2,22 @@
 #include <glfw3.h>
 #include <iostream>
 
+// basic vertex shader
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+// basic fragment shader
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 02.f, 1.0f);\n"
+    "}\0";
+
 
 // Function prototypes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -9,13 +25,11 @@ void processInput(GLFWwindow* window);
 
 int main()
 {
-
-    // initi window hints
+    // init window hints
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
     // create a window
     GLFWwindow* window = glfwCreateWindow(800, 600, "Nebula", NULL, NULL);
@@ -37,13 +51,60 @@ int main()
         return -1;
     }
 
+    // shader section
+    // ----------------------------------------
+
+    // Vertex Shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // attach shader source code to shader object and compile
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for compile-time vertex shader errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog
+            << std::endl;
+    }
+
+    // Fragment Shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for compile-time fragment shader errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog
+            << std::endl;
+    }
+
     // Before we can render we need to tell OpenGL the size of the rendering
     // window
     glViewport(0, 0, 800, 600);
 
+    // triangle vertices
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
 
+    // create a vertex buffer object and bind
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // send vertex data to the buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // start render loop
+    // Render loop
+    // -----------------------------------------------------
     while (!glfwWindowShouldClose(window))
     {
         // input
